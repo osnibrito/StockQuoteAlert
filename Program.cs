e2ae5296f9b7493cbe1ac2d219398921597fd7e6
osnibrito <osni_br@hotmail.com>
2023-12-05 17:02:11 -0300

    feat: sending email to buy or sell

--- Program.cs
+++ Program.cs
@@ -4,13 +4,31 @@ using stock_quote_alert.Interface;
 namespace stock_quote_alert;
 
 internal static class Program{
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
         var inputArgs = new Input(args);
         var configs = Config.GetSetup("./appsettings.json");
         
-        var emailManager = new Email(new Buy(), configs);
-        emailManager.SendEmail(configs, inputArgs, inputArgs.PriceBuy);
+        var api = new Api(configs.ApiToken);
+        while (true)
+        {
+            var actualPrice = await api.GetPriceStock(inputArgs.Stock);
+
+            if (actualPrice >= inputArgs.PriceSell)
+            {
+                Console.WriteLine($"Sending emails to sell {inputArgs.Stock}");
+                var emailManager = new Email(new Sell(), configs);
+                emailManager.SendEmail(inputArgs, inputArgs.PriceSell);
+            }
+            else if (actualPrice <= inputArgs.PriceBuy)
+            {
+                Console.WriteLine($"Sending emails to buy {inputArgs.Stock}");
+                var emailManager = new Email(new Buy(), configs);
+                emailManager.SendEmail(inputArgs, inputArgs.PriceBuy);
+            }
+            await Task.Delay(configs.ApiTimer);
+        }
+        // ReSharper disable once FunctionNeverReturns
     }
 
 }
